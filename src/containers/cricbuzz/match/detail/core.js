530866b2618ad623e505110eb71fe906d41b3df4
Shreyas <shreyas.hande@gmail.com>
2021-04-07 22:33:16 +0530

    showing captain

--- src/containers/cricbuzz/match/detail/core.js
+++ src/containers/cricbuzz/match/detail/core.js
@@ -56,6 +56,30 @@ const styles = theme => ({
 });
 
 class MatchCore extends Component {
+    getPlayerLabel = (playerId) => {
+        let text = this.props.playerMap[playerId];
+
+        let roles = [];
+
+        let isCaptain = false;
+        for (const player of this.props.captains) {
+            if (playerId === player.playerId) {
+                isCaptain = true;
+                break;
+            }
+        }
+
+        if (isCaptain) {
+            roles.push('c');
+        }
+
+        if (roles.length > 0) {
+            text += ' ( ' + roles.join(' & ') + ' ) ';
+        }
+
+        return text;
+    }
+
     handlePlayerClick = playerId => (event) => (this.props.onPlayerClick && this.props.onPlayerClick(playerId));
 
     renderPlayers = () => {
@@ -91,7 +115,7 @@ class MatchCore extends Component {
                 let playerObject = playerObjects[index];
                 markup.push(
                     <Chip
-                        label={this.props.playerMap[playerObject.playerId]}
+                        label={this.getPlayerLabel(playerObject.playerId)}
                         className={this.props.classes.chip}
                         variant="outlined"
                         key={'player_' + playerObject.playerId}
@@ -99,6 +123,8 @@ class MatchCore extends Component {
                     />
                 );
             }
+
+
         }
         return markup;
     };
